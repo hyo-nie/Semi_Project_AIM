2d68bb45e06c146c8d1420c5f88d0ca72aaa70f7
hyowon <111281386+hyo-nie@users.noreply.github.com>
2022-11-18 12:39:40 +0900

    Delete 1118aim directory

--- 1118aim/src/main/java/com/aim/store/action/Action.java
+++
@@ -1,8 +0,0 @@
-package com.aim.store.action;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
